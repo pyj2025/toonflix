71ae4ee641b646eccdb7aa0e3af7dd3f27d44740
“Youngjoon” <pyj2025@gmail.com>
2023-12-19 12:55:28 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String name;
+
+  // Player(this.name, this.id);
+  Player({required this.name});
+}
+
+void main() {
+  // var yj = Player("yj");
+  var human = Player(name: "human");
+
+  human.name;
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // base theme Google = MaterialApp, Apple = CupertinoApp
+    return MaterialApp(
+      // home: Text("Hello world!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Scaffold Appbar!"),
+          centerTitle: false,
+          elevation: 100,
+        ),
+        body: Center(
+          child: Text("Flutter body Center child text!"),
+        ),
+      ),
+    );
+  }
+}
